f1f8ee741c5705596909ae0f2cc02ee9cca99ba6
IKEMURA_KEI <ikemurakei2001@gmail.com>
2023-06-23 18:29:22 +0800

    added distortion

--- nodes/HBV1780Node.cpp
+++ nodes/HBV1780Node.cpp
@@ -4,6 +4,7 @@
 
 #include <sensor_msgs/Image.h>
 #include <cv_bridge/cv_bridge.h>
+#include <filesystem>
 
 #define SHOW_IMG true
 #define PUB_MSG true
@@ -16,8 +17,37 @@ int main(int ac, char **av)
     ros::Time::init();
     ros::NodeHandle nh("~");
 
+    std::string left_camera_config_file = "";
+    if (!nh.getParam("left_camera_config_file", left_camera_config_file))
+    {
+        ROS_FATAL_STREAM("Config file to the left camera not given!");
+    }
+    std::string right_camera_config_file = "";
+    if (!nh.getParam("right_camera_config_file", right_camera_config_file))
+    {
+        ROS_FATAL_STREAM("Config file to the right camera not given!");
+    }
+
+    left_camera_config_file = std::string(realpath(left_camera_config_file.c_str(), NULL));
+    right_camera_config_file = std::string(realpath(right_camera_config_file.c_str(), NULL));
+
+    cv::FileStorage fsLeft(left_camera_config_file, cv::FileStorage::READ);
+    cv::Mat leftCamMat;
+    fsLeft["camera_matrix"] >> leftCamMat;
+    cv::Mat leftDistCoeff;
+    fsLeft["distortion_coefficients"] >> leftDistCoeff;
+
+    cv::FileStorage fsRight(left_camera_config_file, cv::FileStorage::READ);
+    cv::Mat rightCamMat;
+    fsRight["camera_matrix"] >> rightCamMat;
+    cv::Mat rightDistCoeff;
+    fsRight["distortion_coefficients"] >> rightDistCoeff;
+
     ros::Publisher leftImgPub = nh.advertise<sensor_msgs::Image>("left/image_raw", 10);
     ros::Publisher rightImgPub = nh.advertise<sensor_msgs::Image>("right/image_raw", 10);
+
+    ros::Publisher leftImgRectPub = nh.advertise<sensor_msgs::Image>("left/image_rect", 10);
+    ros::Publisher rightImgRectPub = nh.advertise<sensor_msgs::Image>("right/image_rect", 10);
 
     static HBV1780::HBV1780Camera cameraHandle;
     static cv::Mat left;
@@ -33,7 +63,7 @@ int main(int ac, char **av)
 #if SHOW_IMG
     cv::namedWindow("left");
     cv::namedWindow("right");
-    cv::namedWindow("whole");
+    // cv::namedWindow("whole");
 #endif
 
     ros::Time lastTime = ros::Time::now();
@@ -43,15 +73,21 @@ int main(int ac, char **av)
         // rate.sleep();
         cameraHandle.getFrame(left, right, whole);
 
+        cv::Mat undistortedLeft;
+        cv::undistort(left, undistortedLeft, leftCamMat, leftDistCoeff);
+
+        cv::Mat undistortedRight;
+        cv::undistort(right, undistortedRight, rightCamMat, rightDistCoeff);
+
         float dt = (ros::Time::now() - lastTime).toSec();
         float fps = 1 / dt;
         ROS_INFO_STREAM("fps: " << std::to_string(fps));
         lastTime = ros::Time::now();
 
 #if SHOW_IMG
-        cv::imshow("left", left);
-        cv::imshow("right", right);
-        cv::imshow("whole", whole);
+        cv::imshow("left", undistortedLeft);
+        cv::imshow("right", undistortedRight);
+        // cv::imshow("whole", whole);
         char k = cv::waitKey(1);
         if (k == 'q')
             break;
